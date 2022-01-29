2068af89c630769584b6984f08fd8b3f4e75eddb
pablo-the-souza <53923086+pablo-the-souza@users.noreply.github.com>
2022-01-30 05:22:26 +1000

    initial commit

---
+++ Coding.cs
@@ -0,0 +1,9 @@
+namespace coding_tracker
+{
+    internal class Coding
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string Duration { get; set;  }
+    }
+}
